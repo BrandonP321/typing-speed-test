a731b3be2f0d4526db39f68315e18307cd976a6c
Brandon Phillips <brandon.phillips976@gmail.com>
2020-10-24 21:16:59 -0700

    created basic file/folder structure

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+require('dotenv').config();
+
+const db = require('./models');
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./public/'));
+
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 15 * 60 * 1000
+    }
+}));
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+// routes
+require('./controllers/')(app);
+
+
+db.sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+        console.log('App listening on PORT: ' + PORT);
+    });
+});
